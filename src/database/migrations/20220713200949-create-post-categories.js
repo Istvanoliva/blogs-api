75acad620663f327961280f27565c1f5b9589899
Istvan <olivaistvan@gmail.com>
2022-07-13 18:55:31 -0300

    feat: Cria novas migrations

---
+++ src/database/migrations/20220713200949-create-post-categories.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('PostCategories', {
+      postId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'BlogPosts',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      categoryId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,       
+        references: {
+          model: 'Categories',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('PostCategories');
+  }
+};
