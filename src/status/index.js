26ebbcd790f33abe1ec9dcdaa5b81fca5e813874
Istvan <olivaistvan@gmail.com>
2022-07-15 16:50:05 -0300

    feat: (status) Adiciona novos status

--- src/status/index.js
+++ src/status/index.js
@@ -7,6 +7,22 @@ const status = {
         status: 400,
         message: 'Invalid fields',
     },
+    invalidName: { 
+        status: 400,
+        message: '"displayName" length must be at least 8 characters long',
+    },
+    invalidEmail: {
+        status: 400,
+        message: '"email" must be a valid email',
+    },
+    invalidPassword: {
+        status: 400,
+        message: '"password" length must be at least 6 characters long',
+    },
+    emailExist: {
+        status: 409,
+        message: 'User already registered',
+    },
 };
 
 module.exports = status;
