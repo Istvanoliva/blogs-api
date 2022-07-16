ead4a2b853c3f40ea0c783aa194650ff76eef81b
Istvan <olivaistvan@gmail.com>
2022-07-16 00:21:05 -0300

    feat: (userController) Cria funcao createUser

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,12 +1,20 @@
-const loginService = require('../services/userService');
+const userService = require('../services/userService');
 
 const userController = {
     login: async (req, res, next) => {
         const { email, password } = req.body;
 
         try {
-            const token = await loginService.login(email, password);
+            const token = await userService.login(email, password);
             return res.status(200).json({ token });
+        } catch (error) {
+            next(error);
+        }
+    },
+    createUser: async (req, res, next) => {
+        try {
+            const token = await userService.createUser(req.body);
+            return res.status(201).json({ token });
         } catch (error) {
             next(error);
         }
