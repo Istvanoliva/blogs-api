137f82778efd183e63a626fe0313cdf16a495400
Istvan <olivaistvan@gmail.com>
2022-07-20 21:18:22 -0300

    feat: (jwtMiddlewate) Cria funcao verifyUser

--- src/middlewares/jwtMiddleware.js
+++ src/middlewares/jwtMiddleware.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const status = require('../status');
 
+const { BlogPost } = require('../database/models');
+
 const secret = process.env.JWT_SECRET;
 
 const jwtMiddleware = {
@@ -19,6 +21,19 @@ const jwtMiddleware = {
             return res.status(invalidToken.status).json({ message: invalidToken.message }); 
         }
     },
+    verifyUser: async (req, res, next) => {
+        const { unauthorized } = status;
+        const { authorization: token } = req.headers;
+        const { id } = req.params;
+
+        const user = jwt.verify(token, secret);
+        const post = await BlogPost.findByPk(id);
+
+        if (user.data.id !== post.dataValues.userId) {
+            return res.status(unauthorized.status).json({ message: unauthorized.message });
+        }
+        next();
+      },
 }; 
 
 module.exports = jwtMiddleware;
